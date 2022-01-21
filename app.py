38a22ccdd4a69e9a5732c3e8275d816ec8895578
lufire <lukas.feierabend@gmail.com>
2022-01-21 20:23:59 +0100

    re-setup for dockerization

---
+++ app.py
@@ -0,0 +1,9 @@
+# import pemfc_dash
+# import pemfc_dash.main
+from pemfc_dash.main import server
+
+# if __name__ == "__main__":
+#     # [print(num, x) for num, x in enumerate(dl.ID_LIST) ]
+#     app.run(debug=True, use_reloader=False)
+#     # app.run_server(debug=True, use_reloader=False,
+#     #                host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
