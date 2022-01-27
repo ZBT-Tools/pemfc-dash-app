74794d1e0e1c4f8e494d5939f2717d85080b38e4
lufire <lukas.feierabend@gmail.com>
2022-01-27 15:55:33 +0100

    run local server

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 from pemfc_dash.main import app
 server = app.server
 
-# if __name__ == "__main__":
-#     # [print(num, x) for num, x in enumerate(dl.ID_LIST) ]
-#     app.run_server(debug=True, use_reloader=False)
-#     # app.run_server(debug=True, use_reloader=False,
-#     #                host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == "__main__":
+    # [print(num, x) for num, x in enumerate(dl.ID_LIST) ]
+    app.run_server(debug=True, use_reloader=False)
+    # app.run_server(debug=True, use_reloader=False,
+    #                host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
