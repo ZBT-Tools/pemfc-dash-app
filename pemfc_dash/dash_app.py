f21c35541856aa312603bff7ebf1eb2f572e0b6f
kamalizzu <57372828+kamalizzu@users.noreply.github.com>
2022-02-03 23:02:36 +0100

    Update dash_app.py
    minor changes by removing unnecessary transformation

--- pemfc_dash/dash_app.py
+++ pemfc_dash/dash_app.py
@@ -1,12 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
-from dash_extensions.enrich import DashProxy, TriggerTransform, \
-    MultiplexerTransform
+from dash_extensions.enrich import DashProxy, MultiplexerTransform
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = DashProxy(__name__, external_stylesheets=external_stylesheets,
                 suppress_callback_exceptions=True,
-                transforms=[TriggerTransform(), MultiplexerTransform()])
+                transforms=[MultiplexerTransform()])
 
 # app = dash.Dash(__name__, suppress_callback_exceptions=True)
 # server = app.server
