020a321829de06953a0283e4b05872ce770a70bc
lufire <lukas.feierabend@gmail.com>
2022-05-01 01:55:37 +0200

    redis timeout change

--- pemfc_dash/dash_app.py
+++ pemfc_dash/dash_app.py
@@ -27,7 +27,7 @@ caching_backend = RedisStore(
     host='eu2-arriving-honeybee-30031.upstash.io',
     password='8390b37d96074c5ba8b27653e8ec1957',
     port=30031,
-    default_timeout=300)
+    default_timeout=900)
 try:
     caching_backend.delete('test')
 except (redis.exceptions.ConnectionError, ConnectionRefusedError) as E:
