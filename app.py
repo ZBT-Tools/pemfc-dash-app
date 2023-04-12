e4cb6bcb79de4872116765c91b007c18c8f47daa
Kuschel <fkuschel@gmx.de>
2023-04-12 11:26:40 +0200

    Test update der main-function

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pemfc_dash.main import app #, celery_app
 server = app.server
 # celery_app = celery_app
 if __name__ == "__main__":
+    print("Hallo!")
     # [print(num, x) for num, x in enumerate(dl.ID_LIST) ]
     app.run_server(debug=True, use_reloader=False)
     # app.run_server(debug=True, use_reloader=False,
